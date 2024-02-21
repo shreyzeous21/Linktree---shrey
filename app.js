2f26356d3785461115f210c4f8744a2106b60ef6
shreyzeous21 <shrey.sadhukhan21@gmail.com>
2024-02-22 02:39:15 +0530

    main

--- app.js
+++ app.js
@@ -2,13 +2,11 @@ const shareButtons = document.querySelectorAll('.tile-share-button')
 console.log(shareButtons)
 
 async function copyText(e) {
-//prevent button going to the site
     e.preventDefault()
-    const link = this.getAttribute('link')
-    console.log(link)
+    const link = this.getAttribute('link');
     try {
         await navigator.clipboard.writeText(link)
-        alert("Copied the text: " + link)
+        alert("Copied to clipboard: " + link)
     } catch (err) {
         console.error(err)
     }
